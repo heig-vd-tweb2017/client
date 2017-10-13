07a941817411b415f24f440766408b9e47fd3d84
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2017-10-13 19:58:47 +0200

    Users' charts are new correctly updated. Need to refactor

--- src/js/usersChart.js
+++ src/js/usersChart.js
@@ -1,23 +1,27 @@
+const red = 'rgb(255, 99, 132)';
+const orange = 'rgb(255, 159, 64)';
+const yellow = 'rgb(255, 205, 86)';
+const green = 'rgb(75, 192, 192)';
+const blue = 'rgb(54, 162, 235)';
+const purple = 'rgb(153, 102, 255)';
+const grey = 'rgb(201, 203, 207)';
+
 Chart.defaults.global.legend.display = false; // Permet de retirer les labels des dataset
 
 class BarChart {
-  constructor(ctx, data) {
-    this.data = data;
-    this.ctx = ctx;
-    this.place = ['Second', 'First', 'Third'];
-
-    this.barData = {
-      labels: this.place,
-      datasets: [{
-        backgroundColor: [orange, green, yellow],
-        borderColor: [orange, green, yellow],
-        borderWidth: 1,
-        scaleStartValue: 0,
-        data: this.data,
-      }],
-    };
-
-    this.options = {
+  constructor(canvasId, chartData) {
+    this.config = {
+      type: 'bar',
+      data: {
+        labels: ['Second', 'First', 'Third'],
+        datasets: [{
+          backgroundColor: [orange, green, yellow],
+          borderColor: [orange, green, yellow],
+          borderWidth: 1,
+          scaleStartValue: 0,
+          data: chartData,
+        }],
+      },
       options: {
         responsive: true,
         legend: {
@@ -27,7 +31,7 @@ class BarChart {
           xAxes: [{
             stacked: true,
           }],
-          yAxes: [{ 
+          yAxes: [{
             stacked: true,
             ticks: {
               beginAtZero: true,
@@ -37,25 +41,33 @@ class BarChart {
       },
     };
 
-    this.config = {
-      type: 'bar',
-      data: this.barData,
-      options: this.options,
-    };
+    const canvas = document.getElementById(canvasId).getContext('2d')
 
-    this.chart = new Chart(this.ctx, this.config);
+    this.chart = new Chart(canvas, this.config);
   }
 
   updateData(newData) {
-    this.chart.data.datasets.data = newData;
+    const data = {
+      labels: ['Second', 'First', 'Third'],
+      datasets: [{
+        backgroundColor: [orange, green, yellow],
+        borderColor: [orange, green, yellow],
+        borderWidth: 1,
+        scaleStartValue: 0,
+        data: newData,
+      }],
+    };
+
+    this.chart.config.data = data;
     this.chart.update();
   }
 }
 
-const dataOpened = [('Second', 12), ('First', 17), ('Third', 8)];
-const dataClosed = [('Second', 8), ('First', 9), ('Third', 5)];
+const dataClosed = [('Second', 500), ('First', 653), ('Third', 143)];
 
-const chartOpened = new BarChart(document.getElementById('opened-issues-chart').getContext('2d'), dataOpened);
-const chartClosed = new BarChart(document.getElementById('closed-issues-chart').getContext('2d'), dataClosed);
+const chartClosed = new BarChart('closed-issues-chart', dataClosed);
 
-chartClosed.updateData([('Second', 2), ('First', 3), ('Third', 1)]);
+const updatedDataClosed = [('Second', 0), ('First', 1), ('Third', 0)];
+
+
+chartClosed.updateData(updatedDataClosed);
