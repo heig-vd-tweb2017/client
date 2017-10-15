7fd974e5835e8eb093449693f44c6d22b01c8fb6
Michela Zucca <michela.zucca@heig-vd.ch>
2017-10-15 14:47:38 +0200

    custom-table : gestion auto de la taille des données, index et css ajout d'un style

--- src/js/custom-table.js
+++ src/js/custom-table.js
@@ -24,9 +24,11 @@ class tableHtml {
 
     // Constructions des lignes
     for (let i = 0; i < this.data[0].length; i++) {
-      this.myTable += `<tr><td>${this.data[0][i]}</td>`;
-      this.myTable += `<td>${this.data[1][i]}</td>`;
-      this.myTable += `<td>${this.data[2][i]}</td></tr>`;
+      this.myTable += '<tr>';
+      for (let j = 0; j < this.data.length; j++) {
+        this.myTable += `<td>${this.data[j][i]}</td>`;
+      }
+      this.myTable += '</tr>';
     }
     // Construction fin de tableau
     this.myTable += '</table>';
@@ -36,29 +38,31 @@ class tableHtml {
 }
 
 // Test jeux de données fictifs
-const totalTitle = ['Date', 'Issues Opened', 'Issues Closed'];
-const myArray = new Array(3, 3);
+const totalTitle = ['Date', 'Issues Opened', 'Date', 'Issues Closed'];
+const myArray = new Array(4, 3);
 myArray[0] = ['10 oct 2017', '2 octobre 2017', '13 septembre 2017', '4 septembre 2017', '5 aout 2017', '16 juillet 2017', '12 juillet 2017', '8 juillet 2017'];
 myArray[1] = [1, 2.218, 33, 114, 5, 33, 114, 5];
-myArray[2] = [3, 5, 87, 13, 546, 2, 62, 91];
+myArray[2] = ['10 oct 2017', '2 octobre 2017', '13 septembre 2017', '4 septembre 2017', '5 aout 2017', '16 juillet 2017', '12 juillet 2017', '8 juillet 2017'];
+myArray[3] = [1, 2.218, 33, 114, 5, 33, 114, 5];
 const tableTotal = new tableHtml(totalTitle, myArray, 'total-issues-table');
 
 const openedTitle = ['Position', 'Name', 'Issues Oppened'];
 const openedArray = new Array(3, 3);
 openedArray[0] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 openedArray[1] = ['Jean', 'Pierre', 'Loanne', 'Julie', 'Marc', 'Jack', 'Lucie', 'Pascal', 'Josh', 'Marie'];
-openedArray[2] = [85, 70, 65, 50, 46, 35, 24, 20, 19,5];
+openedArray[2] = [85, 70, 65, 50, 46, 35, 24, 20, 19, 5];
 const tableOpened = new tableHtml(openedTitle, openedArray, 'opened-issues-table');
 
 const closedTitle = ['Position', 'Name', 'Issues Closed'];
 const closedArray = new Array(3, 3);
 closedArray[0] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 closedArray[1] = ['Jean', 'Pierre', 'Loanne', 'Julie', 'Marc', 'Jack', 'Lucie', 'Pascal', 'Josh', 'Marie'];
-closedArray[2] = [85, 70, 65, 50, 46, 35, 24, 20, 19,5];
+closedArray[2] = [85, 70, 65, 50, 46, 35, 24, 20, 19, 5];
 const tableClosed = new tableHtml(closedTitle, closedArray, 'closed-issues-table');
 
-const testUpdate = new Array(3, 3);
+const testUpdate = new Array(4, 3);
 testUpdate[0] = ['10 oct 2017', '2 octobre 2017', '13 septembre 2017', '4 septembre 2017', '5 aout 2017', '16 juillet 2017', '12 juillet 2017', '8 juillet 2017'];
 testUpdate[1] = [1, 2, 3, 4, 5, 6, 7, 8];
-testUpdate[2] = [3, 5, 87, 13, 546, 2, 62, 91];
+testUpdate[2] = ['10 oct 2017', '2 octobre 2017', '13 septembre 2017', '4 septembre 2017', '5 aout 2017', '16 juillet 2017', '12 juillet 2017', '8 juillet 2017'];
+testUpdate[3] = [3, 5, 87, 13, 546, 2, 62, 91];
 tableTotal.setData(testUpdate);
